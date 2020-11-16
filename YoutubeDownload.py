2691aeb404c8a074e738265cae2870d1beb13577
NavyaMogili <Navyamogili@gmail.com>
2020-11-16 13:53:46 +0530

    First Commit

--- YoutubeDownload.py
+++ YoutubeDownload.py
@@ -1,11 +1,20 @@
 from __future__ import unicode_literals
 import youtube_dl
-url = input("Enter your URL:")
+ydl_opts = {
+'outtmpl': '/Nightcore/%(title)s'+'.mp4',
+'format':' (bestvideo[width>=?1080]/bestvideo)+bestaudio/best',
+'noplaylist': False,
 
-ydl = youtube_dl.YoutubeDL({
-    'outtmpl': '/tmp/testvideo.mp4',
-    'format':' bestvideo+bestaudio'
-})
-
-ydl.download([url])
+'allsubtitles': False,
+    'writesubtitles': True,
+    'convertsubtitles':True,
+    'subtitleslangs':'en',
+    'postprocessors': [{
+        'key': 'FFmpegSubtitlesConvertor',
+        'format': 'srt',
+    }],
+}
+url = input("Enter your URL: ")
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+	ydl.download([url])
 print("Downloaded!")
